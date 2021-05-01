61dc7c0fa8d939cc0629a0ea00075a85e72dbcf5
Stanley Huang <iamstanleyhuang@gmail.com>
2021-05-01 12:16:48 -0700

    Add handleSubmit that emits validRoom socket event

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,36 +1,58 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 
 const Home = () => {
-  return (
-    <div>
-      Here's the app
-      {/* <div>
-        <h>Logo</h>
-        <h>thinkquiry.io</h>
-      </div>
-      <div>
-        <h>Thinkquiry!</h>
-        <h>Join Room!</h>
-        <input placeholder='Room'></input>
-        <input placeholder='Name'></input>
-        <button>Enter!</button>
-      </div>
-      <div>
-        <h>Join Existing Room As Admin</h>
-        <input placeholder='Room'></input>
-        <input placeholder='NAME TOO?'></input>
-        <input placeholder='password'></input>
-        <button>Enter</button>
-      </div>
-      <div>
-        <h>Create a New Room</h>
-        <input placeholder='Room Name'></input>
-        <input placeholder='Create Password'></input>
-        <button>Enter</button>
-        <div></div>
-      </div> */}
-    </div>
-  );
+	const [room, setRoom] = useState('');
+	const [name, setName] = useState('');
+
+	const socket = io('http://localhost:5000');
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		console.log('in handleSubmit: ', room, name);
+
+		// do MongoDB stuff here..
+
+		if ('room is valid') {
+			socket.emit('validRoom', { room, name });
+		}
+	};
+
+	return (
+		<div>
+			Here's the app
+			<div>
+				<h1>Logo</h1>
+				<h1>thinkquiry.io</h1>
+			</div>
+			<div>
+				<h1>Thinkquiry!</h1>
+				<h1>Join Room!</h1>
+				<form onSubmit={handleSubmit}>
+					<input placeholder="Room" onChange={(e) => setRoom(e.target.value)} />
+					<input placeholder="Name" onChange={(e) => setName(e.target.value)} />
+					<button type="submit" onClick={handleSubmit}>
+						Enter!
+					</button>
+				</form>
+			</div>
+			<div>
+				<h1>Join Existing Room As Admin</h1>
+				<input placeholder="Room"></input>
+				<input placeholder="NAME TOO?"></input>
+				<input placeholder="password"></input>
+				<button>Enter</button>
+			</div>
+			<div>
+				<h1>Create a New Room</h1>
+				<input placeholder="Room Name"></input>
+				<input placeholder="Create Password"></input>
+				<button>Enter</button>
+				<div></div>
+			</div>
+		</div>
+	);
 };
 
 export default Home;
