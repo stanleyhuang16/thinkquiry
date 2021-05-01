b5fb138589d580c35bd606aeb3f36448f3e3c54f
Stanley Huang <iamstanleyhuang@gmail.com>
2021-05-01 01:26:43 -0700

    Add socket disconnect handler and comments

--- server/server.js
+++ server/server.js
@@ -7,16 +7,23 @@ const { Server } = require('socket.io');
 const io = new Server(server);
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '../public/index.html');
+	res.sendFile(__dirname + '../public/index.html');
 });
 
+// Websockets/Socket.io stuff
+// Whenever a user connects, run this
 io.on('connection', (socket) => {
-  console.log('a user connected');
+	console.log('a user connected');
+
+	// Whenever a user disconnects, run this
+	socket.on('disconnect', () => {
+		console.log('a user disconnected');
+	});
 });
 
 // 404 handler
 app.use((req, res) => res.status(404).json({ err: 'Page not found!' }));
 
 server.listen(port, () => {
-  console.log(`Thinkquiry listening at http://localhost:${port}`);
+	console.log(`Thinkquiry listening at http://localhost:${port}`);
 });
