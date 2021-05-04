1aa15cab5dc3ea2330a8a58d3445aa63c9a98202
nhan <nhansense1@gmail.com>
2021-05-03 18:58:24 -0700

    add hooks to handle forms and submit buttons

--- src/containers/TeacherQuestionContainer.jsx
+++ src/containers/TeacherQuestionContainer.jsx
@@ -2,11 +2,7 @@ import { useState, useEffect } from 'react';
 import styled from "styled-components";
 import { useHistory } from 'react-router-dom';
 
-function TeacherQuestionContainer(props) {
-  const [questionType, setQuestionType] = useState('');
-  const [multipleChoiceQuestion, setMultipleChoiceQuestion] = useState('');
-  const [shortQuestion, setShortQuestion] = useState('');
-  const history = useHistory(); // use to redirect after request to server?
+import ToggleSwitch from '../components/ToggleSwitch';
 
   // should move these styled components to a separate file
   const EnterQuestionShort = styled.div`
@@ -21,58 +17,75 @@ function TeacherQuestionContainer(props) {
     color: violet;
   `;
 
-  function handleChangeShort(e) {
-    e.preventDefault();
-    // setShortQuestion(e.target.value);
+  const NewQuestionButton = styled.button`
+  color: violet;
+`;
+
+function TeacherQuestionContainer(props) {
+  const [questionType, setQuestionType] = useState('short answer');
+  const [multipleA, setMultipleA] = useState('Choice A');
+  const [multipleB, setMultipleB] = useState('Choice B');
+  const [multipleC, setMultipleC] = useState('Choice C');
+  const [multipleD, setMultipleD] = useState('Choice D');
+  const [question, setQuestion] = useState('Insert Question Text Here');
+
+  const [editingQuestion, setEditingQuestion] = useState(true);
+
+  function handleChangeQuestion(e) {
+    setQuestion(e.target.value);
   }
 
-  function handleSubmitShort(e) {
+  function handleSubmitQuestion(e) {
     e.preventDefault();
-    // something with emit here, send back to server so it can send to other websockets
-    // history push to another component?
+    setEditingQuestion(false);
   }
 
-  function handleChangeMultiple(e) {
-    e.preventDefault();
-    // setMultipleChoiceQuestion(e.target.value);
+  function handleChangeMultiple(setMultiple, e) {
+    setMultiple(e.target.value)
   }
 
-  function handleSubmitMultiple(e) {
-    e.preventDefault();
-    // something with emit here, send back to server so it can send to other websockets
-    // history push to another component?
+  function handleToggleChange(e) {
+    // e.preventDefault();
+    console.log('handled toggle change');
+    if (questionType === 'short answer') {
+      setQuestionType('multiple choice');
+    } else if (questionType === 'multiple choice') {
+      setQuestionType('short answer');
+    }
+    console.log('state is currently :', questionType);
   }
 
   return (
     <>
       Teacher Question Container
-      {props.roomData.questionType === "multiple" ? (
+      <div>Short Answer<ToggleSwitch handleToggleChange={handleToggleChange}/>Multiple Choice</div>
+      Question type: {questionType}
+      {questionType === "multiple choice" ? (
         <EnterQuestionMultiple>
           <h4>Teacher Enter Multiple Choice</h4>
-          <form onSubmit={handleSubmitMultiple}>
-            <label>
+          <form onSubmit={handleSubmitQuestion}>
+          <label>
               Enter Question
-              <input type='text' name='question' onChange={handleChangeMultiple}>
-              </input>
+              <input type='text' name='multipleChoiceQuestion' value={question} onChange={handleChangeQuestion}></input>
             </label>
             <label>
               Enter Choice A
-              <input type='text' name='choiceA' onChange={handleChangeMultiple}>
+              <input type='text' name='choiceA' value={multipleA} onChange={(e) => handleChangeMultiple(setMultipleA, e)}>
               </input>
             </label>
             <label>
               Enter Choice B
-              <input type='text' name='choiceB' onChange={handleChangeMultiple}>
+              <input type='text' name='choiceB' value={multipleB} onChange={(e) => handleChangeMultiple(setMultipleB, e)}>
               </input>
             </label>
             <label>
               Enter Choice C
-              <input type='text' name='choiceC' onChange={handleChangeMultiple}>
+              <input type='text' name='choiceC' value={multipleC} onChange={(e) => handleChangeMultiple(setMultipleC, e)}>
               </input>
             </label>
             <label>
               Enter Choice D
-              <input type='text' name='choiceD' onChange={handleChangeMultiple}>
+              <input type='text' name='choiceD' value={multipleD} onChange={(e) => handleChangeMultiple(setMultipleD, e)}>
               </input>
             </label>
             <input type='submit' value='Submit Multiple Choice'/>
@@ -80,16 +93,19 @@ function TeacherQuestionContainer(props) {
         </EnterQuestionMultiple>
       ) : (
         <EnterQuestionShort>
-          <h4>Teacher Enter Short</h4>
-          <form onSubmit={handleSubmitShort}>
+          <h4>Teacher Enter Short Answer Question Here</h4>
+          <form onSubmit={handleSubmitQuestion}>
             <label>
               Enter Question
-              <input type='text' name='shortQuestion' onChange={handleChangeShort}></input>
+              <input type='text' name='shortQuestion' value={question} onChange={handleChangeQuestion}></input>
             </label>
             <input type='submit' value='Submit Short Answer'/>
           </form>
         </EnterQuestionShort>
       )}
+      {!editingQuestion && <NewQuestionButton>
+        Ask Another Question
+      </NewQuestionButton>}
     </>
   );
 }
