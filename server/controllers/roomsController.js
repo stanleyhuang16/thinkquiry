1ed4a34df19d8a8f23db1b8a6b7b8578bf30e9cd
Stanley Huang <iamstanleyhuang@gmail.com>
2021-05-04 19:17:08 -0700

    Create roomsController with createRoom and checkRoom middlewares

---
+++ server/controllers/roomsController.js
@@ -0,0 +1,30 @@
+const Room = require('../models/model');
+
+const roomsController = {};
+
+roomsController.createRoom = (req, res) => {
+	const { roomName, adminPassword } = req.body;
+
+	Room.create({ roomName, adminPassword })
+		.then((room) =>
+			room
+				? res.status(200).json(room)
+				: res.status(418).json({ error: 'Failed to create room!' })
+		)
+		.catch((err) => res.status(500).json(err));
+};
+
+roomsController.checkRoom = (req, res) => {
+	const { roomName } = req.params;
+
+	Room.findOne({ roomName })
+		.exec()
+		.then((room) =>
+			room
+				? res.status(200).json(room)
+				: res.status(418).json({ error: 'No room found by that name!' })
+		)
+		.catch((err) => res.status(500).json(err));
+};
+
+module.exports = roomsController;
