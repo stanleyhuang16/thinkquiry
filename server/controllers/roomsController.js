b9e0f81805f246ac091aea34c8e5e2bd75113e0a
Stanley Huang <iamstanleyhuang@gmail.com>
2021-05-06 00:48:10 -0700

    Check if room exists first before creating room

--- server/controllers/roomsController.js
+++ server/controllers/roomsController.js
@@ -2,28 +2,15 @@ const Room = require('../models/model');
 
 const roomsController = {};
 
-roomsController.createRoom = (req, res) => {
-	const { roomName, adminPassword } = req.body;
-
-	Room.create({ roomName, adminPassword })
-		.then((room) =>
-			room
-				? res.status(200).json(room)
-				: res.status(500).json({ err: 'Failed to create room!' })
-		)
-		.catch((err) => res.status(500).json(err));
-};
-
-roomsController.checkRoom = (req, res) => {
+roomsController.checkRoom = (req, res, next) => {
 	const { roomName } = req.body;
 
 	Room.findOne({ roomName })
 		.exec()
-		.then((room) =>
-			room
-				? res.status(200).json(room)
-				: res.status(404).json({ err: 'No room found by that name!' })
-		)
+		.then((room) => {
+			if (room) res.locals.room = room;
+			return next();
+		})
 		.catch((err) => res.status(500).json(err));
 };
 
@@ -32,14 +19,21 @@ roomsController.checkRoomAdmin = (req, res) => {
 
 	Room.findOne({ roomName })
 		.exec()
-		.then((room) => {
-			// check if user input matches admin password
-			if (adminPassword === room?.adminPassword) {
-				res.status(200).json(room);
-			} else {
-				res.status(404).json({ err: 'No room found by that name!' });
-			}
-		})
+		.then((room) =>
+			adminPassword === room?.adminPassword
+				? res.status(200).json(room)
+				: res.status(404).json({ err: 'No room found by that name!' })
+		)
+		.catch((err) => res.status(500).json(err));
+};
+
+roomsController.createRoom = (req, res) => {
+	if (res.locals.room) return res.json({ err: 'Room already exists!' });
+
+	const { roomName, adminPassword } = req.body;
+
+	Room.create({ roomName, adminPassword })
+		.then((room) => (room ? res.status(200).json(room) : res.status(400)))
 		.catch((err) => res.status(500).json(err));
 };
 
